88214850ca325aab993fc598a2f4193c47b1fd81
gulzar <ali.gulzar@tuni.fi>
2020-11-30 16:32:11 +0500

    Render displat after connecting to the websocket and fetching sitename

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,43 @@ import Loading from './components/Loading';
 
 const SITE_KEY = 'needed-endurable-plough'
 
-const socket = io.connect('https://api.mountkelvin.com/', {
-  reconnectionDelay: 1000,
-  reconnectionDelayMax: 3000,
-  transports: ['websocket'],
-})
-
-socket.on('connect', () => {
-    socket.emit('subscribe', { siteKey: SITE_KEY })
-})
-
 const App: React.FC = () => {
 
   const [siteName, setSiteName] = React.useState(''); 
   const [loading, setLoading] = React.useState(true);
-  const scenes = ['allOn', 'allOn:70', 'allOn:30', 'allOff']
+  const scenes: Array<string> = ['allOn', 'allOn:70', 'allOn:30', 'allOff']
+
+  let socket: SocketIOClient.Socket;
+
+  const fetchSiteNameAndConnectToWebSocket = async () => {
+    setLoading(true);
+    const response = await axios.get(`https://api.mountkelvin.com/v1/site/${SITE_KEY}`)
+    socket =  io.connect('https://api.mountkelvin.com/', {
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 3000,
+      transports: ['websocket'],
+    })
+
+    socket.on('connect', () => {
+        socket.emit('subscribe', { siteKey: SITE_KEY })
+        setLoading(false)
+    }) 
+    setSiteName(response.data.name)
+  }
 
   React.useEffect(() => {
     setLoading(true)
-    const fetchSiteName = async () => {
-      const response = await axios.get(`https://api.mountkelvin.com/v1/site/${SITE_KEY}`)
-      setSiteName(response.data.name)
-      setLoading(false)
-    }
-    fetchSiteName()
+    fetchSiteNameAndConnectToWebSocket()
   },[])
 
   const setScene = async (scence: Scence) => {
-    socket.emit('apply/scene', scence)
+    socket.on('siteKeyFound', () => {
+      socket.emit('apply/scene', scence)
+    })
+    socket.on('noSuchSiteKey', () => {
+      fetchSiteNameAndConnectToWebSocket()
+      socket.emit('apply/scene', scence)
+    })
     console.log(scence)
   };
 
--- src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const scenes: Array<string> = ['allOn', 'allOn:70', 'allOn:30', 'allOff']
