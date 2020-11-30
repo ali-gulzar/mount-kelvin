8fcb516597a340caa9c89c27fea67fa19d5266a8
gulzar <ali.gulzar@tuni.fi>
2020-11-30 15:07:28 +0500

    Use style components library to make square and circle diagrams

--- src/components/SceneButton.tsx
+++ src/components/SceneButton.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { Scence } from '../types';
 import { getBrightness } from '../helpers';
 
@@ -8,7 +10,47 @@ interface props {
     applySceneFunction: (scene: Scence) => void 
 }
 
+interface SampleProps {
+    height: number
+}
+
+const Circle = styled.div<SampleProps>`
+width: 50px;
+height: 50px;
+border-radius: 25px;
+border:1px solid black;
+position: relative;
+overflow: hidden;
+&:after {
+    ${({height}) => {
+        if (height !== 0) return 'content: "";'
+    }}
+    position:absolute;
+    background:black;
+    border:1px solid black;
+    bottom:0;
+    left:0; 
+    right: 0;
+    height:${({height}) => height}%;
+}
+`
+
+const Square = styled.div`
+    width:100px;
+    height:100px;
+    border:1px solid black;
+    border-radius: 5px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 10px;
+`
+
 const SceneButton: React.FC<props> = ({ scene, siteKey, applySceneFunction }) => {
+
 
     const { brightness, brightnessText } = getBrightness(scene)
 
@@ -20,10 +62,10 @@ const SceneButton: React.FC<props> = ({ scene, siteKey, applySceneFunction }) =>
     }
 
     return (
-        <div className="button" onClick={() => applySceneFunction(sceneValue)}>
-            <div className="circle"/>
+        <Square onClick={() => applySceneFunction(sceneValue)}>
+            <Circle height={brightness}/>
             <p className="sceneText">{brightnessText}</p>
-        </div>
+        </Square>
     )
 }
 
